ced557da87fa360d1ee46fae9c86f8d8b2222727
fabrizzio-gz <fabrizzio.gz@gmail.com>
2024-09-24 11:59:55 -0400

    feat:astroconfig: remove prefetch, image optimization, markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,13 @@
 import { defineConfig } from "astro/config";
-import { remarkModifiedTime } from "./src/utils/remark-modified-time.mjs";
 import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://verse-astro.vercel.app/",
-  prefetch: {
-    prefetchAll: true,
-    defaultStrategy: "viewport",
-  },
 
   experimental: {
     contentCollectionCache: true,
-  },
-
-  image: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**.unsplash.com",
-      },
-    ],
-  },
-
-  markdown: {
-    remarkPlugins: [remarkModifiedTime],
   },
 
   integrations: [
