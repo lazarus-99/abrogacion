07b7d62b49ea443e217d626f755ef410e9c9ac74
fabrizzio-gz <fabrizzio.gz@gmail.com>
2024-09-24 14:51:48 -0400

    feat:consts add more deputies

--- src/consts.ts
+++ src/consts.ts
@@ -20,6 +20,24 @@ interface Deputy {
 
 export const DEPUTIES: Array<Deputy> = [
   {
+    name: "Verónica Challco Tapia",
+    party: "MAS",
+    img: "https://diputados.gob.bo/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2022/01/02-Veronica-Challco-Tapia-Primera-Viceprecidencia-e1724947461210.jpg.webp",
+    isSupportive: false,
+  },
+  {
+    name: "Laura Luisa Nayar Sosa",
+    party: "CC",
+    img: "https://diputados.gob.bo/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2022/01/03-Laura-Luisa-Nayar-Sosa-Segunda-Viceprecidencia-e1724947569329.jpg.webp",
+    isSupportive: true,
+  },
+  {
+    name: "Rosario García Onofre",
+    party: "MAS",
+    img: "https://diputados.gob.bo/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2022/01/04-Rosario-Garcia-Onofre-Primera-Secretaria-e1724947642630.jpg.webp",
+    isSupportive: true,
+  },
+  {
     name: "María René Álvarez Camacho",
     party: "CREEMOS",
     img: "https://diputados.gob.bo/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2022/01/07-Maria-Rene-Alvarez-Camacho-Cuarta-Secretaria-e1724948019386.jpg.webp",
