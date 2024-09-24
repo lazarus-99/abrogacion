15cb61ad88e61a8d1eb47dddd4e5d487a4075df5
fabrizzio-gz <fabrizzio.gz@gmail.com>
2024-09-24 12:21:22 -0400

    feat:consts: add DEPUTIES
    Used as source of truth for deputies info

--- src/consts.ts
+++ src/consts.ts
@@ -10,3 +10,25 @@ export const HeaderLinks = [
   { href: "/posicion/a-favor", title: "A favor" },
   { href: "/posicion/en-contra", title: "En contra" },
 ];
+
+interface Deputy {
+  name: string;
+  party: string;
+  img: string;
+  isSupportive: boolean;
+}
+
+export const DEPUTIES: Array<Deputy> = [
+  {
+    name: "María René Álvarez Camacho",
+    party: "CREEMOS",
+    img: "https://diputados.gob.bo/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2022/01/07-Maria-Rene-Alvarez-Camacho-Cuarta-Secretaria-e1724948019386.jpg.webp",
+    isSupportive: true,
+  },
+  {
+    name: "Alba Moira Osinaga Rivero",
+    party: "MAS",
+    img: "https://diputados.gob.bo/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2022/01/78-Alba-Moira-Osinaga-Rivero-SANTA-CRUZ-2-e1724965456691.jpg.webp",
+    isSupportive: false,
+  },
+];
