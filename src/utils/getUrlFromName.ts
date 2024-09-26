cacae483b9b31e91c64a5cd1ad2549685c0d2d33
fabrizzio-gz <fabrizzio.gz@gmail.com>
2024-09-26 14:37:58 -0400

    feat:utils remove unused utils

--- src/utils/getUrlFromName.ts
+++
@@ -1,7 +0,0 @@
-// Normalize name for URL: María Álvarez -> maria-alvarez
-export const getUrlFromName = (name: string) =>
-  name
-    .normalize("NFD")
-    .replace(/\p{Diacritic}/gu, "")
-    .replace(/\s/g, "-")
-    .toLowerCase();
