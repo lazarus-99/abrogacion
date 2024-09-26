56dcdec26e26ce6c991394870abe7fffb851ae00
fabrizzio-gz <fabrizzio.gz@gmail.com>
2024-09-26 14:34:06 -0400

    feat:consts use json data for deputies

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,5 @@
+import RAW_DEPUTIES from "./data/deputies.json";
+
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
@@ -12,41 +14,13 @@ export const HeaderLinks = [
 ];
 
 interface Deputy {
+  url: string;
   name: string;
-  party: string;
+  uri: string;
+  email?: string;
   img: string;
-  isSupportive: boolean;
+  party: string;
+  isSupportive?: boolean;
 }
 
-export const DEPUTIES: Array<Deputy> = [
-  {
-    name: "Verónica Challco Tapia",
-    party: "MAS",
-    img: "https://diputados.gob.bo/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2022/01/02-Veronica-Challco-Tapia-Primera-Viceprecidencia-e1724947461210.jpg.webp",
-    isSupportive: false,
-  },
-  {
-    name: "Laura Luisa Nayar Sosa",
-    party: "CC",
-    img: "https://diputados.gob.bo/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2022/01/03-Laura-Luisa-Nayar-Sosa-Segunda-Viceprecidencia-e1724947569329.jpg.webp",
-    isSupportive: true,
-  },
-  {
-    name: "Rosario García Onofre",
-    party: "MAS",
-    img: "https://diputados.gob.bo/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2022/01/04-Rosario-Garcia-Onofre-Primera-Secretaria-e1724947642630.jpg.webp",
-    isSupportive: true,
-  },
-  {
-    name: "María René Álvarez Camacho",
-    party: "CREEMOS",
-    img: "https://diputados.gob.bo/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2022/01/07-Maria-Rene-Alvarez-Camacho-Cuarta-Secretaria-e1724948019386.jpg.webp",
-    isSupportive: true,
-  },
-  {
-    name: "Alba Moira Osinaga Rivero",
-    party: "MAS",
-    img: "https://diputados.gob.bo/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2022/01/78-Alba-Moira-Osinaga-Rivero-SANTA-CRUZ-2-e1724965456691.jpg.webp",
-    isSupportive: false,
-  },
-];
+export const DEPUTIES: Array<Deputy> = RAW_DEPUTIES;
