1119b4fc58a62bf4c097344e4af87086490324a6
fabrizzio-gz <fabrizzio.gz@gmail.com>
2024-09-26 15:44:12 -0400

    feat: change page name to "Incendios Bolivia"

--- src/consts.ts
+++ src/consts.ts
@@ -4,7 +4,7 @@ import RAW_DEPUTIES from "./data/deputies.json";
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // Base Page Metadata, src/layouts/BaseLayout.astro
-export const SITE_TITLE = "Abrogación";
+export const SITE_TITLE = "Incendios Bolivia";
 export const SITE_DESCRIPTION = "An Astro Theme for Visual Storytelling";
 
 // Header Links, src/components/Header.astro
